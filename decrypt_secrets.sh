a2c77e4eac9d413098a32b93b20dbd7e2b9f317b
Ryan Freeman <hello@ryanfreeman.dev>
2024-08-20 22:06:35 +0100

    update workflow

--- decrypt_secrets.sh
+++ decrypt_secrets.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output ./.env ./.env.gpg
+--output ./secrets ./secrets.gpg
